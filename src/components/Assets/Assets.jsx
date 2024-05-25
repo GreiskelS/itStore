ce6cc20b8c365a09eca1557242753fff97875c77
Greiskel <greiskel.sanchez@gmail.com>
2024-05-25 14:04:03 +0200

    Assets

--- src/components/Assets/Assets.jsx
+++ src/components/Assets/Assets.jsx
@@ -10,10 +10,10 @@ export const urlgift = "https://github.com/GreiskelS/Assets.proyects/blob/main/a
 export const urlcreators = "https://github.com/GreiskelS/Assets.proyects/blob/main/assets/DSC08400.JPG?raw=true";
 
 //Botons Home
-export const buttonlike = "src/assets/like.png";
-export const buttonsearchnav = "src/assets/lupa.png";
-export const buttonlanguage = "src/assets/bandera.png";
-export const buttonkart = "src/assets/kart.png";
+export const buttonlike = "https://github.com/GreiskelS/Assets.proyects/blob/main/assets/like.png?raw=true";
+export const buttonsearchnav = "https://github.com/GreiskelS/Assets.proyects/blob/main/assets/lupa.png?raw=true";
+export const buttonlanguage = "https://github.com/GreiskelS/Assets.proyects/blob/main/assets/bandera.png?raw=true";
+export const buttonkart = "https://github.com/GreiskelS/Assets.proyects/blob/main/assets/kart.png?raw=true";
 
 //Logo
-export const logo = "https://cdn.glitch.global/55101d5a-1c75-498d-8bf8-5a3bd396a138/Logo.png?v=1712071547142";
+export const logo = "https://cdn.glitch.global/55101d5a-1c75-498d-8bf8-5a3bd396a138/Logo.png";
